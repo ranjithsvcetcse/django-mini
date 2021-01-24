442cfa8706b733886514bc3985470410eba2acec
ranjith <ranjithsvcetcse.com>
2021-01-24 21:18:40 +0530

    modified settings, deleted setup & sqlite3

--- RHG/miniproject/miniproject/settings.py
+++ RHG/miniproject/miniproject/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1j8jgfbg-3sig#j78n9hx138geg1w02pyht@%skv*&-d^@f66w'
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' # It is kept secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
