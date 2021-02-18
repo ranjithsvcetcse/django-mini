f32febc84f2a23f8f28750e63fe4eeb0ff610249
ranjith <ranjithsvcetcse.com>
2021-02-18 22:47:11 +0530

    Version 0.0.1 Release New

--- RHG/miniproject/mini/urls.py
+++ RHG/miniproject/mini/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
 	path('ide/', views.IDE.as_view(), name = 'mini_IDE'),
 	path('dashboard/', views.Dashboard.as_view(), name = 'mini_dashboard'),
+	path('ide/<str:file>/', views.IDE.as_view(), name = 'mini_IDE_file'),
+	path('logout/', views.logout_view, name = 'mini_logout'),
 ]
--- RHG/miniproject/mini/views.py
+++ RHG/miniproject/mini/views.py
@@ -1,85 +1,162 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.contrib.auth.mixins import LoginRequiredMixin
-import os
 from miniproject.settings import BASE_DIR
-
+from django.contrib.auth import logout
+import os
 
 class IDE(LoginRequiredMixin, View):
 	'''
-		Class comments
+		The UI powered editing environment. Admin login is mandatory to access this page.
+		User may need to login with Admin login credentials to continue to this page.
 	'''
-
 	login_url = '/admin/login/'
 	redirect_field_name = 'next'
 	html_template_file = 'mini/IDE.html'
-
 	# model = model_name
 	# form = form_name
 	
 	context = {}
 	context['page_name'] = 'IDE'
 
-	def get(self, request):
-		IDE_DEFAULT_content = "<h2><table><tr><td><p></td><td><p>Rich Text Editor is the industry-leading online html editor. It enables content contributors easily create and publish HTML anywhere: on the desktop and on mobile.</p></td></tr><tr><td><p></td><td><p>Rich Text Editor toolbar is completely configurable and it is also effortless to implement. You just need a couple lines of code to add this editor to web applications.</p></td></tr></table>"
-		print(IDE_DEFAULT_content)
-		return render(request, self.html_template_file, locals())
+	def get(self, request, file = 'default.html'):
+		# print(file)
+		try:
+			base_template_path = Dashboard.template_path
+			formdata = {'app_name': 'mini', 'file_name': file}
+			self.context['formdata'] = formdata
+			# print('succes')
+		except:
+			base_template_path = os.path.join(BASE_DIR, 'mini\\templates\\mini')
+
+		file_abs_path = os.path.join(base_template_path, file)
+		# print(file_abs_path)
+
+		with open(file_abs_path, 'r') as f:
+			l=f.read()
+			# print(l)
+			self.context['IDE_DEFAULT_content'] = self.remove_new_lines(l)
+			f.close()
+		# print(self.context['IDE_DEFAULT_content'])
+
+		return render(request, self.html_template_file, context = self.context)
 
 	def post(self, request):
+
 		app_name = request.POST['app_name']
 		file_name = request.POST['file_name']
 		content = request.POST['html_code']
-		self.context['IDE_DEFAULT_content'] = content
+		formdata = {'app_name': app_name, 'file_name': file_name}
+		self.context['formdata'] = formdata
+
 		app = app_name + '\\templates\\' + app_name
 		file = file_name + '.html'
 		local_path = os.path.join(app, file)
 		file_path = os.path.join(BASE_DIR, local_path)
 
+		# print(app,file,BASE_DIR, file_path)
+		# print(content, request.POST,app_name)
+
 		try:
 			with open(file_path, 'w') as fp:
+				# code = '<!DOCTYPE html><html><head><title></title></head><body>' + content + '</body></html>'
+				# code = self.add_newline(content)
 				fp.write(content)
 				fp.close()
 				message = 'File saved : ' + file_path
 		except:
 			message = 'Something went wrong! Check all file names are correct.'
-
+		
+		self.context['IDE_DEFAULT_content'] = self.escape_chars(content, ["'"])
 		self.context['message'] = message
+
 		return render(request, self.html_template_file, context=self.context)
+
+	def escape_chars(self, code, charlist):
+		temp, esc = '', '\\'
+		for _ in code:
+			temp += esc + _ if _ in charlist else _
+		return temp
+
+	def add_newline(self, code):
+		temp, nl = '', '\n'
+		for _ in code:
+			temp += nl + _ if _ == '<' else _
+		return temp
+
+	def remove_new_lines(self, code):
+		temp, nl = '', '<br>'
+		for _ in code:
+			temp += nl if _ == '\n' else _
+		return temp
+
+	
 
 class Dashboard(LoginRequiredMixin, View):
 	'''
-		Class comments
+		Dashboard contains a list of html files in the app's template folder.
+		The app name is required as input.
 	'''
-
 	login_url = '/admin/login/'
 	redirect_field_name = 'next'
 	html_template_file = 'mini/dashboard.html'
-
 	# model = model_name
 	# form = form_name
-
 	context = {}
 	context['page_name'] = 'Dashboard'
-	file_data = []
+	# file_data = ['']
 	
 	def get(self, request):
-		self.getListOfFiles('')
-		self.context['files'] =  self.file_data
+		
+		# self.getListOfFiles('')
+		# self.context['files'] =  self.file_data
 		return render(request, self.html_template_file, context = self.context)
 
 	def post(self, request):
-		# Post method
-		return
+		app_name = request.POST['app_name']
+		# print(app_name+'\\templates')
+		self.template_path = os.path.join(BASE_DIR, app_name+'\\templates\\'+app_name)
+		pre_defined_files = ['default.html' ,'dashboard.html', 'IDE.html', 'layout.html', 'header.html', 'footer.html']
+		try:
+			all_files, error =  self.getListOfFiles(app_name+'\\templates\\'+app_name)
+			# print(all_files[0]['files'])
+			# print(all_files)
+			# print(pre_defined_files)
+			# a=[i if i not in pre_defined_files else None for i in all_files[0]['files']]
+			a=[]
+			for _ in all_files[0]['files']:
+				if _ not in pre_defined_files:
+					a.append(_)
+			all_files[0]['files'] = a
+			self.context['files'] = all_files
+
+		except:
+			error = 'Invalid app : Cannot find ' + app_name 
+		# print(self.file_data)
+		self.context['error'] = error
+		error=''
+		return render(request, self.html_template_file, context = self.context) 
 
 	def getListOfFiles(self, Name):
 		dirName = os.path.join(BASE_DIR, Name)
+		# print(dirName)
 		l = len(BASE_DIR.split('\\'))
-		print(os.walk(dirName, topdown=False))
-		for root, dirs, files in os.walk(dirName, topdown=False):
+		# print(os.walk(dirName, topdown=False))
+		file_data = []
+		for root, dirs, files in os.walk(dirName, topdown = False):
 			root = '\\'.join(root.split('\\')[l:])
+			file_data.append({'root': root,'dirs': dirs, 'files': files})
+			# print(root,dirs,files)
+		if len(file_data) <= 0:
+			error = Name + ' is empty.'
+		else:
+			error = 'No error'
+		# print(file_data)
+		return file_data, error
 
-			self.file_data.append({'root': root,'dirs': dirs, 'files': files})
-			
+def logout_view(request):
+    logout(request)
+    return redirect('mini_IDE')
 
